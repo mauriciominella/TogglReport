199f0d3177f3ae5a3503451957bb262debce1629
mauricio.minella@bravi.com.br <mauricio.minella@bravi.com.br>
2015-01-22 22:21:38 -0200

    Password removed from the code

--- TogglReport.Domain/Repository/TimeEntryRepositoryHarvest.cs
+++ TogglReport.Domain/Repository/TimeEntryRepositoryHarvest.cs
@@ -30,7 +30,7 @@ namespace TogglReport.Domain.Repository
         {
             UserSettingsService userSettingsService = new UserSettingsService();
 
-            _provider = new RestRequestProvider("bravi", "mauricio.minella@bravi.com.br", "Maidenmba1060");
+            _provider = new RestRequestProvider("bravi", "mauricio.minella@bravi.com.br", "xxxxxx");
             _repository = new Harvest.Api.Repository(_provider);
 
             MapHarvestEntityToTimeEntryEntity();
